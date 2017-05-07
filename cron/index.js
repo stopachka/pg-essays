6f8d85b84e41b55334fffedfd414457e896d9bae
stopachka <stepan.p@gmail.com>
2017-05-07 12:01:35 -0700

    sm

--- cron/index.js
+++ cron/index.js
@@ -1,21 +1,27 @@
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const cp = require('child_process');
 
 const ROOT_PATH = 'http://www.paulgraham.com';
 const ARTICLES_INDEX = `${ROOT_PATH}/articles.html`;
 
 const BOOK_STARTER = `
-  <html>
-    <body>
-    </body>
-  </html>
+<!doctype html>
+  <html lang="en">
+  <head>
+  <meta charset="utf-8" />
+  <title>Essays by Paul Gragam</title>
+  <link rel="stylesheet" href="style.css"  type="text/css" />
+  </head>
+  <body>
+  <!-- Your book goes here -->
+  </body>
+</html>
 `;
 
-function fetchHtml(articlesIndex) {
-  return fetch(articlesIndex)
-    .then(res => res.text())
-    .then(text => cheerio.load(text));
+function fetchHtml(url) {
+  return fetch(url).then(res => res.text()).then(text => cheerio.load(text));
 }
 
 function toLinks($) {
@@ -25,7 +31,7 @@ function toLinks($) {
     .find('a')
     .toArray()
     .map(node => node.attribs.href)
-    .filter(href => href.indexOf('http') === -1) // if the link is relative then it's an essay PG wrote on the blog
+    .filter(href => href.indexOf('http') === -1)
     .map(path => `${ROOT_PATH}/${path}`)
     .reverse(); // earlier first
 }
@@ -41,30 +47,56 @@ function removeLogo($) {
   return $;
 }
 
-function toChapter($) {
-  return [removeMenu, removeLogo].reduce(($, f) => f($), $);
+function removeApplyYC($) {
+  $link = $('a[href="http://ycombinator.com/apply.html"]').toArray()[0];
+  $link && $link.closest('table').remove();
+  return $;
 }
 
-function buildBook($book, $chapter) {
-  $book('body').append('<hr />').append($chapter('body'));
+function addPageBreak($) {
+  $.append('<mbp:pagebreak />');
+}
+
+function toChapter($body) {
+  return [removeMenu, removeLogo, removeApplyYC, addPageBreak].reduce(
+    ($, f) => f($),
+    $body
+  );
+}
+
+function buildChapters($book, html) {
+  $book('body').append(html);
   return $book;
 }
 
-function run(articlesIndex) {
-  fetchArticles(articlesIndex)
+function run(articlesIndex, bookStarter) {
+  fetchHtml(articlesIndex)
     .then(toLinks)
     .then(links =>
       Promise.all(
-        links.map(link => fetchHtml(link).then(html => [link, html]))
+        links
+          .slice(0, 3)
+          .map(link => fetchHtml(link).then($html => [link, $html('body')]))
       ))
     .then(linksWithHtml =>
       linksWithHtml
-        .map(([link, html]) => toChapter(html))
-        .reduce(buildBook, cheerio.load(BOOK_STARTER)));
+        .map(([link, $body]) => toChapter($body))
+        .map($chapter => $chapter('body').html())
+        .reduce(buildChapters, cheerio.load(bookStarter)))
+    .then(writeChapters)
+    .then(buildMobi);
 }
+
+// run(ARTICLES_INDEX, BOOK_STARTER);
 
 // ----------------------------------------------------------
 // Dev
+
+function buildMobi(pathToHtml, pathToMobi) {
+  cp.execSync(`~/kindlegen ${pathToHtml} -verbose -o ${pathToMobi}`);
+}
+
+buildMobi(`${__dirname}/chapters.html`, `chapters.mobi`);
 
 function writeChapters($) {
   fs.writeFileSync('chapters.html', $.html());
@@ -76,12 +108,11 @@ function writeLinks(xs) {
 function readLinks() {
   return JSON.parse(fs.readFileSync('links.json', 'utf-8'));
 }
-
-const linksWithHtml = readLinks();
-
-const bookHTML = linksWithHtml
-  .map(([link, html]) => toChapter(cheerio.load(html)))
-  .slice(0, 10)
-  .reduce(buildBook, cheerio.load(BOOK_STARTER));
-
-fs.writeFileSync('book.html', bookHTML.html());
+// const linksWithHtml = readLinks();
+//
+// const bookHTML = linksWithHtml
+//   .map(([link, html]) => toChapter(cheerio.load(html)))
+//   .map(chapt)
+//   .slice(0, 10)
+//   .reduce(buildBook, cheerio.load(BOOK_STARTER));
+// fs.writeFileSync('book.html', bookHTML.html());
