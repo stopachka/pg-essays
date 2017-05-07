8af1657b08013ca77090d0587ccca20c1c650576
stopachka <stepan.p@gmail.com>
2017-05-07 14:18:35 -0700

    sm

--- cron/index.js
+++ cron/index.js
@@ -3,20 +3,80 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const cp = require('child_process');
 
+const BOOK_TITLE = 'Essays by Paul Graham';
 const ROOT_PATH = 'http://www.paulgraham.com';
 const ARTICLES_INDEX = `${ROOT_PATH}/articles.html`;
+const BOOK_DIR = 'book';
+const COVER_PATH = `cover.jpg`;
+const HTML_PATH = `index.html`;
+const NCX_PATH = `ncx.xml`;
+const OPF_PATH = `opf.xml`;
+const TOC_ID = 'toc';
 
 const BOOK_STARTER = `
-<!doctype html>
-  <html lang="en">
-  <head>
-  <meta charset="utf-8" />
-  <title>Essays by Paul Graham</title>
-  </head>
-  <body>
-  </body>
-</html>
+  <!doctype html>
+    <html lang="en">
+    <head>
+    <meta charset="utf-8" />
+    <title>${BOOK_TITLE}</title>
+    </head>
+    <body>
+    </body>
+  </html>
 `;
+
+const OPF = `
+  <?xml version="1.0" encoding="iso-8859-1"?>
+  <package unique-identifier="uid" xmlns:opf="http://www.idpf.org/2007/opf" xmlns:asd="http://www.idpf.org/asdfaf">
+    <metadata>
+      <dc-metadata  xmlns:dc="http://purl.org/metadata/dublin_core" xmlns:oebpackage="http://openebook.org/namespaces/oeb-package/1.0/">
+        <dc:Title>${BOOK_TITLE}</dc:Title>
+        <dc:Language>en</dc:Language>
+        <dc:Creator>Paul Graham</dc:Creator>
+        <dc:Copyrights>Paul Graham</dc:Copyrights>
+        <dc:Publisher>Stepan Parunashvili</dc:Publisher>
+        <x-metadata>
+          <EmbeddedCover>${COVER_PATH}</EmbeddedCover>
+        </x-metadata>
+      </dc-metadata>
+    </metadata>
+    <manifest>
+        <item id="content" media-type="text/x-oeb1-document" href="${HTML_PATH}#${TOC_ID}"></item>
+        <item id="ncx" media-type="application/x-dtbncx+xml" href="${NCX_PATH}"/>
+    </manifest>
+    <spine toc="ncx">
+        <itemref idref="content"/>
+    </spine>
+  </package>
+`;
+
+function buildNcx(linksWithChapters) {
+  const toNav = ([link, $chapter], idx) => `
+    <navPoint id="${chapterId(link)}" playOrder="${2 + idx}">
+      <navLabel>
+        <text>${chapterTitle(link, $chapter)}</text>
+      </navLabel>
+    </navPoint>
+  `;
+  return `
+    <?xml version="1.0"?>
+    <!DOCTYPE ncx PUBLIC "-//NISO//DTD ncx 2005-1//EN"
+      "http://www.daisy.org/z3986/2005/ncx-2005-1.dtd">
+    <ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
+    <head>
+    </head>
+     <docTitle>
+       <text>${BOOK_TITLE}</text>
+     </docTitle>
+     <navMap>
+       <navPoint id="${TOC_ID}" playOrder="1">
+         <navLabel><text>Table of Contents</text></navLabel>
+         <content src="${HTML_PATH}#${TOC_ID}" />
+       </navPoint>
+       ${linksWithChapters.map(toNav)}
+   </ncx>
+  `;
+}
 
 function fetchHtml(url) {
   return fetch(url).then(res => res.text()).then(text => cheerio.load(text));
@@ -34,67 +94,88 @@ function toLinks($) {
     .reverse(); // earlier first
 }
 
-function removeMenu($) {
+function chapterId(link) {
+  return link;
+}
+
+function removeMenu(link, $body) {
   // TODO(stopachka) -- best way to remove the first td
   $('td:first-child').toArray()[0].children = [];
   return $;
 }
 
-function removeLogo($) {
+function removeLogo(link, $body) {
   $('a[href="index.html"]').remove();
   return $;
 }
 
-function removeApplyYC($) {
+function removeApplyYC(link, $body) {
   $link = $('a[href="http://ycombinator.com/apply.html"]').toArray()[0];
   $link && $link.closest('table').remove();
   return $;
 }
 
-function addPageBreak($) {
+function addPageBreak(link, $body) {
   $.append('<mbp:pagebreak />');
+  return $;
 }
 
-function toChapter($body) {
-  return [removeMenu, removeLogo, removeApplyYC, addPageBreak].reduce(
-    ($, f) => f($),
-    $body
-  );
+function replaceChapterTitle($) {
+  $firstImageWithAlt = $('img').toArray().find(x => x.attribs.alt);
+  $firstImageWithAlt
+    .parent()
+    .append(
+      `<h1 id={${chapterId($link)}}>${firstImageWithAlt.attribts.alt}</h1>`
+    );
+  $firstImageWithAlt.remove();
+  return $;
+}
+
+function toChapter(link, $body) {
+  return [
+    removeMenu,
+    removeLogo,
+    removeApplyYC,
+    addPageBreak,
+    replaceChapterTitle
+  ].reduce(($, f) => f(link, $), $body);
+}
+
+function chapterTitle(link, $chapter) {
+  return $chapter(`#${chapterId($link)}`).toArray()[0].textContent;
 }
 
 function buildChapters($book, html) {
-  $book('body').append(html);
   return $book;
 }
 
-function run(articlesIndex, bookStarter) {
-  fetchHtml(articlesIndex)
+function buildBook(linksWithChapters) {}
+
+function buildMobi(linksWithChapters) {
+  const [ncx, toc, html] = [buildNcx(linksWithBody), buildToc, buildBook(linksWithBody)];
+}
+
+function run() {
+  fetchHtml(ARTICLES_INDEX)
     .then(toLinks)
     .then(links =>
       Promise.all(
         links
           .slice(0, 3)
-          .map(link => fetchHtml(link).then($html => [link, $html('body')]))
+          .map(link =>
+            fetchHtml(link).then($html => [
+              link,
+              toChapter(link, $html('body'))
+            ]))
       ))
-    .then(linksWithHtml =>
-      linksWithHtml
-        .map(([link, $body]) => toChapter($body))
-        .map($chapter => $chapter('body').html())
-        .reduce(buildChapters, cheerio.load(bookStarter)))
-    .then(writeChapters)
     .then(buildMobi);
 }
-
-// run(ARTICLES_INDEX, BOOK_STARTER);
 
 // ----------------------------------------------------------
 // Dev
 
-function buildMobi(pathToHtml, pathToMobi) {
-  cp.execSync(`~/kindlegen ${pathToHtml} -verbose -o ${pathToMobi}`);
-}
-
-buildMobi(`${__dirname}/chapters.html`, `chapters.mobi`);
+// cp.execSync(`~/kindlegen ${pathToHtml} -verbose -o ${pathToMobi}`);
+// buildMobi(`${__dirname}/chapters.html`, `chapters.mobi`);
 
 function writeChapters($) {
   fs.writeFileSync('chapters.html', $.html());
