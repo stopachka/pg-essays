21f2540aa1941a17aabcb0b4a90c2193a4292415
stopachka <stepan.p@gmail.com>
2025-05-24 17:21:08 -0700

    cleanedEssaysHTML

--- src/transform.ts
+++ src/transform.ts
@@ -1,6 +1,8 @@
 import pLimit from "p-limit";
 import * as cheerio from "cheerio";
 import path from "path";
+import crypto from "node:crypto";
+
 // ----------------
 // Utils
 
@@ -78,8 +80,123 @@ async function loadArticleIndex(): Promise<IndexEntry[]> {
 // -------------
 // cleanEssayHTML
 
-function cleanEssayHTML($: cheerio.CheerioAPI): string {
-  return $.html();
+function removeMenu($: cheerio.CheerioAPI) {
+  $("td:first-child").toArray()[0].children = [];
+}
+
+function removeLogo($: cheerio.CheerioAPI) {
+  $('a[href="index.html"]').remove();
+}
+
+function removeHr($: cheerio.CheerioAPI) {
+  $("hr").remove();
+}
+
+function removeApplyYC($: cheerio.CheerioAPI) {
+  $('font:contains("Want to start a startup")')
+    .last()
+    .closest("table")
+    .remove();
+}
+
+function removeTitleImage($: cheerio.CheerioAPI) {
+  const $firstImageWithAlt = $("img[alt]").first();
+  $firstImageWithAlt.remove();
+}
+
+function replaceTables($: cheerio.CheerioAPI) {
+  const toDiv = (tag: string) =>
+    $(tag)
+      .toArray()
+      .reverse()
+      .forEach(function (x) {
+        $(x).replaceWith(`<div>${$(x).html()}</div>`);
+      });
+  ["td", "td", "tbody", "thead", "table"].forEach(toDiv);
+  return $;
+}
+
+const badImages = new Set(["http://www.virtumundo.com/images/spacer.gif"]);
+async function localiseImages($: cheerio.CheerioAPI) {
+  const toLocalName = (url: string) => {
+    const ext = path.extname(new URL(url).pathname) || ".jpg";
+    const key = crypto.createHash("md5").update(url).digest("hex");
+    return `${key}${ext}`;
+  };
+
+  await Promise.all(
+    $("img[src]")
+      .toArray()
+      .filter((n) => n.attribs.src.includes("http"))
+      .map(async (node) => {
+        const remote = node.attribs.src;
+        if (badImages.has(remote)) {
+          console.log(`[asset-cache] remove: ${remote}`);
+          $(node).remove();
+          return;
+        }
+        const filename = toLocalName(remote);
+
+        const dest = path.join(
+          import.meta.dir,
+          "..",
+          "cache",
+          "assets",
+          filename
+        );
+        const file = Bun.file(dest);
+        const fileExists = await file.exists();
+        if (!fileExists) {
+          console.log(`[asset-cache] miss: ${remote}`);
+          const res = await limitedFetch(remote);
+          const buf = Buffer.from(await res.arrayBuffer());
+          await Bun.write(file, buf);
+        } else {
+          console.log(`[asset-cache] hit: ${remote}`);
+        }
+
+        node.attribs.src = `../assets/${filename}`;
+      })
+  );
+}
+
+function removeFontTags($: cheerio.CheerioAPI) {
+  $("font").each((_, el) => {
+    const inner = $(el).html();
+    $(el).replaceWith(inner || "");
+  });
+}
+
+function removeOuterTags($: cheerio.CheerioAPI) {
+  $("script").remove();
+  $("head").remove();
+  $("body").replaceWith($("body").html() || "");
+  $("html").replaceWith($("html").html() || "");
+}
+
+const stringifiedTitle = (title: string) => title.replace(/\//g, "_");
+
+async function cleanEssayHTML(
+  entry: IndexEntry,
+  idx: number,
+  $: cheerio.CheerioAPI
+) {
+  return withCache(
+    "cleanedEssayHTML",
+    `${idx}_${stringifiedTitle(entry.title)}.html`,
+    async () => {
+      removeMenu($);
+      removeLogo($);
+      removeTitleImage($);
+      removeApplyYC($);
+      removeHr($);
+      removeFontTags($);
+      replaceTables($);
+      removeOuterTags($);
+      await localiseImages($);
+      return $.html();
+    }
+  );
 }
 
 // -------------
@@ -88,11 +205,13 @@ function cleanEssayHTML($: cheerio.CheerioAPI): string {
 async function main() {
   const index = await loadArticleIndex();
   const with$ = await Promise.all(
-    index.map(async (entry) => {
+    index.map(async (entry, idx) => {
       const $ = await loadHTML(entry.url);
+      const cleanedHTMLString = await cleanEssayHTML(entry, idx, $);
       return {
         ...entry,
         $,
+        cleanedHTMLString,
       };
     })
   );
